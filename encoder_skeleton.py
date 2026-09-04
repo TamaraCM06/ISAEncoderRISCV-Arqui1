e78e0a198def72830554c4bb074ff89ebdac86f3
Tamara Cajiao <167063588+TamaraCM06@users.noreply.github.com>
2026-09-04 00:00:49 -0600

    parseo de registros en ambos formatos ABI y Numerico

--- encoder_skeleton.py
+++ encoder_skeleton.py
@@ -30,6 +30,53 @@ instrucciones = {
     "bne": {"formato": "B", "opcode": 0b1100011, "funct3": 0b001},
 }
 
+def parse_reg(reg_str: str) -> int:
+    # Parsea el registro con nombre abi a el registro en hardware
+    reg_str = reg_str.strip().lower()
+    abi_names = {
+        "zero": 0,
+        "ra": 1,
+        "sp": 2,
+        "gp": 3,
+        "tp": 4,
+        "t0": 5,
+        "t1": 6,
+        "t2": 7,
+        "s0": 8,
+        "fp": 8,
+        "s1": 9,
+        "a0": 10,
+        "a1": 11,
+        "a2": 12,
+        "a3": 13,
+        "a4": 14,
+        "a5": 15,
+        "a6": 16,
+        "a7": 17,
+        "s2": 18,
+        "s3": 19,
+        "s4": 20,
+        "s5": 21,
+        "s6": 22,
+        "s7": 23,
+        "s8": 24,
+        "s9": 25,
+        "s10": 26,
+        "s11": 27,
+        "t3": 28,
+        "t4": 29,
+        "t5": 30,
+        "t6": 31,
+    }
+    if reg_str in abi_names:
+        return abi_names[reg_str]
+    # Obtener solamente el numero en caso de registro en formato xN
+    if reg_str.startswith("x") and reg_str[1:].isdigit():
+        val = int(reg_str[1:])
+        if 0 <= val <= 31:
+            return val
+    raise ValueError(f"Registro inválido: '{reg_str}'")
+
 def encode_instruction(instruction: str) -> int:
     """
     Recibe una instrucción como texto, p. ej. "add x5, x6, x7", y debe
